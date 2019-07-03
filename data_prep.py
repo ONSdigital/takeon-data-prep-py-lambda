e034a1a2268f0a0d0dbe590104b5e683785984a8
RyanKilbane <RyanKilbane92@hotmail.com>
2019-07-03 23:00:55 +0100

    Refactored data prep to move database connection into it's own class instead of inside of the select method

--- data_prep.py
+++ data_prep.py
@@ -10,6 +10,28 @@ logger.setLevel(logging.INFO)
 lambda_client = boto3.client('lambda')
 
 
+class Connection:
+    def __init__(self):
+        self.db_user = os.getenv("DATABASE_USER")
+        self.db_pw = os.getenv("DATABASE_PASSWORD")
+        self.db_host = os.getenv("DATABASE_HOST")
+        self.db_name = os.getenv("DATABASE_NAME")
+        self.connection =  psycopg2.connect(database=self.db_name,
+                                    user=self.db_user,
+                                    password=self.db_pw,
+                                    host=self.db_host,
+                                    port='5432')
+    
+    def __entry__(self):
+        return self.connection.cursor()
+
+    def __exit__(self, type, value, exception):
+        if exception is None:
+            pass
+        else:
+            logger.error("Error during select: {}".format(exception))
+        self.connection.close()
+
 class DataPrep:
     def __init__(self, event):
         self.event = event
@@ -19,46 +41,21 @@ class DataPrep:
         self.instance = self.event["instance"]
         self.questionCode = "questionCode"
         self.response = "response"
-        self.db_user = os.getenv("DATABASE_USER")
-        self.db_pw = os.getenv("DATABASE_PASSWORD")
-        self.db_host = os.getenv("DATABASE_HOST")
-        self.db_name = os.getenv("DATABASE_NAME")
         self.wrangler_lambda = os.getenv("WRANGLER_NAME")
 
     def get_qcode_resp_from_db(self):
 
-        # print(db_user)
-        # print(db_pw)
-        # print(db_host)
-        try:
-            conn = psycopg2.connect(database=self.db_name,
-                                    user=self.db_user,
-                                    password=self.db_pw,
-                                    host=self.db_host,
-                                    port='5432')
-            cur = conn.cursor()
-            # Print PostgreSQL Connection properties
-            # print(conn.get_dsn_parameters(), "\n")
-            cur.execute("""SELECT trim(questioncode), trim(response)
+        with Connection() as connection:
+            connection.execute("""SELECT trim(questioncode), trim(response)
                           FROM dev01.response 
                           WHERE reference = %s AND period = %s AND survey = %s """,
-                        (self.reference, self.period, self.survey))
-            record = cur.fetchall()
-            # print("You are connected to - ", record, "\n")
+                          (self.reference, self.period, self.survey))
+            record = connection.fetchall()
+
             qcode_resps = []
             for row in record:
                 qcode_resps.append({self.questionCode: row[0], self.response: row[1]})
             self.event.update({'responses': qcode_resps})
-
-        except (Exception, psycopg2.Error) as error:
-            logger.error("ERROR:{}".format(error))
-            sys.exit()
-        finally:
-            # closing database connection.
-            if (conn):
-                cur.close()
-                conn.close()
-                logger.info("DataPrep PostgreSQL connection is closed")
 
     def send_data_to_wrangler(self):
         try:
